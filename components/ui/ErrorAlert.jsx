cb4a27b9cba5c827da63c60ba311081ae4590ad6
Martin Yonkov Dimitrov <mdimi@softserveinc.com>
2022-07-29 11:48:09 +0300

    clean project

--- components/ui/ErrorAlert.jsx
+++
@@ -1,7 +0,0 @@
-import classes from './ErrorAlert.module.css';
-
-function ErrorAlert({children,  ...props}) {
-  return <div className={classes.alert}>{children}</div>;
-}
-
-export default ErrorAlert;
